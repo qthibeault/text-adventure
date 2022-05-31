b8d4602888d044a5132f294f874e9590fb4e0f02
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-05-31 09:52:00 -0700

    Implement basic game loop
    Create set of actions that a player can take. Create method to parse player input into actions. Create a method to update the game state iteratively until the player or the enemy dies

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,43 @@
 module Main where
 
+import System.IO
+import Text.Printf (printf)
+import Data.Char (toLower)
+
+data GameAction = Attack | Run
+
+data GameState = GameState {
+    playerHealth :: Int,
+    enemyHealth :: Int
+}
+
+lowerString :: String -> String 
+lowerString = map toLower
+
+parseAction :: String -> GameAction
+parseAction "attack" = Attack
+parseAction "run" = Run
+parseAction _ = error "Invalid action"
+
+readAction :: IO GameAction
+readAction = parseAction . lowerString <$> getLine
+
+playerPrompt :: GameState -> IO GameAction 
+playerPrompt state = do
+    putStr prompt
+    hFlush stdout
+    readAction
+    where prompt = printf "Player: %d/Enemy: %d>" (playerHealth state) (enemyHealth state)
+
+step :: GameState -> IO ()
+step state
+    | playerHealth state <= 0 = putStrLn "YOU DIED"
+    | enemyHealth state <= 0 = putStrLn "GREAT ENEMY DEFEATED"
+    | otherwise = do
+        action <- playerPrompt state
+        case action of Attack -> step GameState { playerHealth = playerHealth state - 1, enemyHealth = enemyHealth state - 2 }
+                       Run -> putStrLn "Fled"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = step initialState
+    where initialState = GameState { playerHealth = 10, enemyHealth = 8 }
